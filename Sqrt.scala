6d2cb77baecea2510f35fde3ada1430d27a8b231
U-bhatti-PC\bhatti <bhatti@bhatti-PC.(none)>
2013-05-18 21:36:33 -0700

    improved sqrt

--- Sqrt.scala
+++ Sqrt.scala
@@ -1,16 +1,18 @@
 // uses Newton's method
 object Sqrt {
     def abs(x: Double) = if (x >= 0) x else -x
-    def sqrtIter(guess: Double, x: Double): Double =
-        if (isGoodEnough(guess, x)) guess 
-        else sqrtIter(improve(guess, x), x) 
-    def isGoodEnough(guess: Double, x: Double) =
-        abs(guess * guess - x) / x < 0.0001 // note we divide by x so that it works for both large and small numbers
 
-    def improve(guess: Double, x: Double) =
-        (guess + x / guess) / 2
+    def sqrt(x: Double) = {
+        def sqrtIter(guess: Double, x: Double): Double =
+            if (isGoodEnough(guess, x)) guess 
+            else sqrtIter(improve(guess, x), x) 
+        def isGoodEnough(guess: Double, x: Double) =
+            abs(guess * guess - x) / x < 0.0001 // note we divide by x so that it works for both large and small numbers
 
-    def sqrt(x: Double) = sqrtIter(1.0, x)
+        def improve(guess: Double, x: Double) =
+            (guess + x / guess) / 2
+        sqrtIter(1.0, x)
+    }
 
 }
 
