2de8173475d9ff4488f305f1cdb59a49856450bb
U-bhatti-PC\bhatti <bhatti@bhatti-PC.(none)>
2013-05-19 16:56:43 -0700

    improved sqrt

--- Sqrt.scala
+++ Sqrt.scala
@@ -3,15 +3,15 @@ object Sqrt {
     def abs(x: Double) = if (x >= 0) x else -x
 
     def sqrt(x: Double) = {
-        def sqrtIter(guess: Double, x: Double): Double =
-            if (isGoodEnough(guess, x)) guess 
-            else sqrtIter(improve(guess, x), x) 
-        def isGoodEnough(guess: Double, x: Double) =
+        def sqrtIter(guess: Double): Double =
+            if (isGoodEnough(guess)) guess 
+            else sqrtIter(improve(guess)) 
+        def isGoodEnough(guess: Double) =
             abs(guess * guess - x) / x < 0.0001 // note we divide by x so that it works for both large and small numbers
 
-        def improve(guess: Double, x: Double) =
+        def improve(guess: Double) =
             (guess + x / guess) / 2
-        sqrtIter(1.0, x)
+        sqrtIter(1.0)
     }
 
 }
