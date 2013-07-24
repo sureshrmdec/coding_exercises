64b634a6acbaf51d336e1be4d46da62c347c4056
U-bhatti-PC\bhatti <bhatti@bhatti-PC.(none)>
2013-07-23 22:46:07 -0700

    string examples

---
+++ Permutation.java
@@ -0,0 +1,24 @@
+public class Permutation { 
+    void permute( String str ){
+        int length = str.length();
+        boolean[] used = new boolean[ length ];
+        StringBuffer out = new StringBuffer();
+        char[] in = str.toCharArray();
+        doPermute( in, out, used, length, 0 );
+    }
+    void doPermute( char[] in, StringBuffer out,
+        boolean[] used, int length, int level ){
+        if( level == length ){
+            System.out.println( out.toString() );
+            return;
+        }
+        for( int i = 0; i < length; ++i ){
+            if( used[i] ) continue;
+            out.append( in[i] );
+            used[i] = true;
+            doPermute( in, out, used, length, level + 1 );
+            used[i] = false;
+            out.setLength( out.length() - 1 );
+        }
+    }
+}
