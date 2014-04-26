44ba1cc7556d6ffe42852fa74ef5abf6c76b0122
Shahzad Bhatti <bhatti@plexobject.com>
2014-04-25 20:26:11 -0700

    added comments to function8

--- java8/Function8.java
+++ java8/Function8.java
@@ -1,5 +1,17 @@
 import java.util.function.*;
 
+
+//Monoid
+//  allows concatenation of data
+//  concat(empty, x) == x
+//  concat(x, empty) == x
+//  concat(concat(x, y), z) == concat(x, concat(y, z))
+//
+//Monad
+//  compose(f, compose(g, h) == compose(compose(f, g), h)
+//  compose(f, result) == f
+//  compose(result, f) == f
+
 public class Function8 {
   public static void main(String[] args) {
     BinaryOperator<Integer> adder = (n1, n2) -> n1 + n2;
