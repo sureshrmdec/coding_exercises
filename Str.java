5a8d79fc5d55a46c71ecfe6d10281a26ed8bc797
Shahzad Bhatti <bhatti@plexobject.com>
2014-06-24 21:11:10 -0700

    str

---
+++ Str.java
@@ -0,0 +1,25 @@
+public class Str {
+  public static String strStr(String haystack, String needle) {
+
+    int needleLen = needle.length();
+    int haystackLen = haystack.length();
+
+    if (needleLen == haystackLen && needleLen == 0) return "";
+    if (needleLen == 0) return haystack;
+
+    for (int i = 0; i < haystackLen; i++) {
+      if (haystackLen - i + 1 < needleLen) return null;
+
+      int k=i;
+      int j=0;
+      for (;j < needleLen && k < haystackLen && needle.charAt(j) == haystack.charAt(k); j++, k++) {
+      }
+      if (j == needleLen) return haystack.substring(i);
+    }
+
+    return null;
+  }
+  public static void main(String[] args) {
+    System.out.println(strStr(args[0], args[1]));
+  }
+}
