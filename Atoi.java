9627fd99bf6a9c0a285c60cf81eac0577ad37ef9
Shahzad Bhatti <bhatti@plexobject.com>
2014-06-24 18:14:35 -0700

    atoi

---
+++ Atoi.java
@@ -0,0 +1,36 @@
+public class Atoi {
+  public static int atoi(String str) {
+    if (str == null) return 0;
+
+    str = str.trim();
+    if (str.length() == 0) return 0;
+
+    boolean negative = false;
+
+    long result = 0;
+
+    for (int i=0; i<str.length(); i++) {
+      char ch = str.charAt(i);
+      if (i == 0 && ch == '+') {
+        negative = false;
+      } else if (i == 0 && ch == '-') {
+        negative = true;
+      } else if (ch >= '0' && ch <= '9') {
+        result = result * 10 + (ch - '0');
+      } else {
+        throw new NumberFormatException(str + " is not a valid number");
+      }
+    }
+
+    if (negative) result = -result;
+
+    if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+
+    if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+    return (int) result;
+  }
+  public static void main(String[] args) {
+    System.out.println(atoi(args[0]));
+  }
+}
