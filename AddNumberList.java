956f2bed1e5baecae777d78d91b5bcfe00e745f4
Shahzad Bhatti <sbhatti@peak6.come>
2014-06-30 16:33:53 -0700

    examples

---
+++ AddNumberList.java
@@ -0,0 +1,50 @@
+public class AddNumberList {
+   static class Node {
+      int val;
+      Node next;
+      Node(int x) {
+         val = x;
+         next = null;
+      }
+      Node add(int x) {
+         next = new Node(x);
+         return next;
+      }
+      public String toString() {
+         return val + (next == null ? "" : "->" + next.toString());
+      }
+   }
+   public static Node add(Node l1, Node l2) {
+      Node result = new Node(0);
+      Node l3 = result;
+      int carry = 0;
+      while (l1 != null || l2 != null) {
+         if (l1 != null) {
+            carry += l1.val;
+            l1 = l1.next;
+         }
+         if (l2 != null) {
+            carry += l2.val;
+            l2 = l2.next;
+         }
+         l3.next = new Node(carry % 10);
+         l3 = l3.next;
+         carry /= 10;
+      }
+      if (carry != 0) {
+         l3.next = new Node(carry);
+      }
+      return result.next;
+   }
+   public static void main(String[] args) {
+      Node l1 = new Node(2);
+      l1.add(4).add(3).add(1);
+      Node l2 = new Node(5);
+      l2.add(6).add(4);
+      Node result = add(l1, l2);
+      System.out.println(l1);
+      System.out.println(l2);
+      System.out.println(result);
+   }
+
+}
