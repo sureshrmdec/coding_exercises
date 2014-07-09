259da6b0f45afe0554f1972cf81cf3499ed9b57a
Shahzad Bhatti <sbhatti@peak6.come>
2014-07-09 16:56:35 -0700

    samples

--- TreeNode.java
+++ TreeNode.java
@@ -134,14 +134,14 @@ public class TreeNode {
       Stack<TreeNode> stack = new Stack<TreeNode>();
       TreeNode p = root;
 
-      while(p != null || !stack.empty()){
+      while(p != null || stack.size() > 0){
          if(p.right != null){
             stack.push(p.right);
          }
          if(p.left != null){
             p.right = p.left;
             p.left = null;
-         }else if(!stack.empty()){
+         }else if(stack.size() > 0){
             TreeNode temp = stack.pop();
             p.right=temp;
          }
