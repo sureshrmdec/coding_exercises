2c919253cfcafe5007b30cece483a9bcb4929e8e
Shahzad Bhatti <bhatti@plexobject.com>
2014-09-15 19:52:41 -0700

    examples

---
+++ RotateBinSearch.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+public class RotateBinSearch {
+  public static int search(int[] a, int x) {
+    return search(a, 0, a.length-1, x);
+  }
+  public static int search(int[] a, int l, int u, int x) {
+    while (l <= u) {
+      int m = (l+u) / 2;
+      System.out.println("l " + l + ", m " + m + ", u " + u);
+      if (x == a[m]) {
+        return m;
+      } else if (a[l] <= a[m]) {
+        if (x > a[m]) {
+          l = m+1;
+        } else if (x >= a[l]) {
+          u = m-1;
+        } else {
+          l = m + 1;
+        }
+      } else if (x < a[m]) u = m-1;
+      else if (x <= a[u]) l = m+1;
+      else u = m - 1;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(search(new int[] {3, 4, 5, 6, 1, 2}, 2));
+  }
+}
+
--- SubTree.java
+++ SubTree.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
+
 public class SubTree {
   private class Node {
     private int data;
+    private Node next;
     private Node left;
     private Node right;
     public Node(int data) {
@@ -28,5 +32,55 @@ public class SubTree {
     return (matchTree(r1.left, r2.left) &&
         matchTree(r1.right, r2.right));
   } 
+
+  public Node nthToLast(Node head, int n) { 
+    if (head == null || n < 1) {
+      return null;
+    }
+    Node p1 = head;
+    Node p2 = head;
+    for (int j = 0; j < n - 1; ++j) { // skip n-1 steps ahead
+      if (p2 == null) return null; // not found since list size < n 
+      p2 = p2.next; 
+    }
+
+    while (p2.next != null) { 
+      p1 = p1.next;
+      p2 = p2.next; 
+    }
+    return p1; 
+  }
+  public Node addList(Node l1, Node l2, int carry) {
+    if (l1 == null && l2 == null) return null;
+    Node result = new Node(carry);
+    int value = carry;
+    if (l1 != null) value += l1.data;
+    if (l2 != null) value += l2.data;
+    result.data = value % 10;
+    Node more = addList(l1 == null ? null : l1.next, l2 == null ? null : l2.next, value > 10 ? 1 : 1);
+    result.next = more;
+    return result;
+  }
+
+  void findSum(Node head, int sum, ArrayList<Integer> buffer,
+      int level) {
+    if (head == null) return;
+    int tmp = sum;
+    buffer.add(head.data);
+    for (int i = level;i >- 1; i--){
+      tmp -= buffer.get(i);
+      if (tmp == 0) print(buffer, i, level);
+    }
+    ArrayList<Integer> c1 = (ArrayList<Integer>) buffer.clone();
+    ArrayList<Integer> c2 = (ArrayList<Integer>) buffer.clone();
+    findSum(head.left, sum, c1, level + 1);
+    findSum(head.right, sum, c2, level + 1);
+  }
+  void print(ArrayList<Integer> buffer, int level, int i2) {
+    for (int i = level; i <= i2; i++) {
+      System.out.print(buffer.get(i) + " ");
+    }
+    System.out.println();
+  }
 }
 
